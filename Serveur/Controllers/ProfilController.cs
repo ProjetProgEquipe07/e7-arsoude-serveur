633962519845abcb6341a6167cee4bcb2efdf332
Laurent A <1680229@cegepmontpetit.ca>
2024-02-06 10:20:20 -0500

    FCT : Appel serveur pour profil

---
+++ Serveur/Controllers/ProfilController.cs
@@ -0,0 +1,31 @@
+﻿using arsoudeServeur.Models;
+using arsoudeServeur.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace arsoudeServeur.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class ProfilController : BaseController
+    {
+        public ProfilController(UtilisateursService utilisateursService) : base(utilisateursService)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Utilisateur>> GetUtilisateurCourant()
+        {
+            var utilisateur = UtilisateurCourant;
+            if (utilisateur == null)
+            {
+                return NotFound(new { Error = "L'utilisateur est introuvable" }); 
+            }
+
+            return Ok(utilisateur); 
+        }
+
+    }
+}
