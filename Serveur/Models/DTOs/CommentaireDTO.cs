d06e4f6e8212784f98d2583748cbed9a8cd8e0b4
JOJO <2041360@cegepmontpetit.ca>
2024-02-23 13:02:37 -0500

    Fix : commentaire

--- Serveur/Models/DTOs/CommentaireDTO.cs
+++ Serveur/Models/DTOs/CommentaireDTO.cs
@@ -5,6 +5,7 @@ namespace arsoudeServeur.Models.DTOs
     public class CommentaireDTO
     {
         public int randonneeId { get; set; }
+        [MaxLength(255, ErrorMessage = "La longueur maximale est de 255 caractères.")]
         public string message { get; set; }
         [Range(0, 5)]
         public int note { get; set; }
